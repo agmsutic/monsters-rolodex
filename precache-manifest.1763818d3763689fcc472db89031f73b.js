0e6ce7528684b4945d327fdf647d69471be70570
agmsutic <agm.sutic@gmail.com>
2019-09-20 20:50:15 +0200

    Updates

---
+++ precache-manifest.1763818d3763689fcc472db89031f73b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4690af96a3a6a9bccce1c4c9fdc4c7b6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52455bc68d86366ab052",
+    "url": "/monsters-rolodex/static/css/main.62907665.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "52455bc68d86366ab052",
+    "url": "/monsters-rolodex/static/js/main.95c20972.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
